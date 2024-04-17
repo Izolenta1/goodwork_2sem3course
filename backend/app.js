f7ca476de95af8ce9fbdf817619482f71f488606
Izolenta1 <geor.novickoff@yandex.ru>
2024-04-18 00:46:28 +0300

    Тест документации

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,32 @@
+/**
+ * @namespace backend.app
+ * @memberOf backend
+ * @description
+ * Модуль, содержащий методы для запуска сервера приложения
+ */
+
 import express from "express";
-  
-const app = express(); 
+
+/**
+ * Переменная, содержащая объект сервера
+ */
+const app = express();
+
+/**
+ * Номер порта
+ * @type {int}
+ */
 const PORT = 3001;
-  
-app.listen(PORT, (error) =>{ 
-    if(!error) 
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
-    else 
-        console.log("Error occurred, server can't start", error); 
-    } 
+
+/**
+ * @name listen
+ * @description
+ * Вызов функции запуска сервера
+ */
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log("Server is Successfully Running, and App is listening on port " + PORT)
+    else
+        console.log("Error occurred, server can't start", error);
+}
 );
